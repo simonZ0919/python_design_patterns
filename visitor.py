803ea7c6af9c83fff4b88b4d36891a0547893f2f
simon <xuehuizhu05@gmail.com>
2020-01-14 00:08:14 +0800

    visitor pattern

---
+++ visitor.py
@@ -0,0 +1,38 @@
+"""
+visitor pattern
+"""
+import abc
+
+
+class Visitable(object):
+    def accept(self, visitor):
+        visitor.visit(self)
+
+
+class CompositeVisitable(Visitable):
+    def __int__(self, iterable):
+        self.iterable = iterable
+
+    def accept(self, visitor):
+        for element in self.iterable:
+            element.accept(visitor)
+
+        visitor.visit(self)
+
+
+class AbstractVisitor(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def visit(self, element):
+        raise NotImplementedError("visit method is not defined")
+
+
+class ConcreteVisible(Visitable):
+    def __init__(self):
+        pass
+
+
+class ConcreteVisitor(AbstractVisitor):
+    def visit(self, element):
+        pass
